2c24c7be5f13e93d086517c8f3cf09b5f990292c
Nick-Holdem <dorinr038@gmail.com>
2022-12-08 09:14:49 +0200

    add

--- src/Main.java
+++ src/Main.java
@@ -7,13 +7,13 @@ import java.io.IOException;
 public class Main {
     public static void main(String[] args) throws IOException {
 
-        System.out.println("\t> Hello Words\n");
-        hello salut = new hello();
-        salut.hello();
-
-        System.out.println("\t> txt operation\n");
-        StringUsage text = new StringUsage();
-        text.operation();
+//        System.out.println("\t> Hello Words\n");
+//        hello salut = new hello();
+//        salut.hello();
+//
+//        System.out.println("\t> txt operation\n");
+//        StringUsage text = new StringUsage();
+//        text.operation();
 
         System.out.println("\n> Basic Calculator\n");
         calc str = new calc();
